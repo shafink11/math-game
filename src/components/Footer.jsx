56a31257db08b6bed8432f3596ced53edf687d72
shafink11 <102826872+shafink11@users.noreply.github.com>
2022-09-16 18:44:36 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="App-footer">
-          <small>made with ♥ by <a href="https://github.com/ogaston" rel="noopener noreferrer" target="_blank">Omar Gaston</a></small>
+          <small>made with ♥ by <a href="https://github.com/ogaston" rel="noopener noreferrer" target="_blank">Code for Good Team 17</a></small>
         </footer>
     )
 }
